69df50a5692a9cd69849fadcb401118e4059b17b
3sfour <ali.alasfour@siu.edu>
2025-11-17 17:48:20 -0600

    added a player class just made the skeleton also touched up the isntaller it stll has two magic numbers i need to fix but ill do that tommorrow.

--- installer.py
+++ installer.py
@@ -1,18 +1,24 @@
-import requests
 import os
-from config import INSTALL_PATH
 import zipfile
 import shutil
 import hashlib
 
+import requests
+
+from config import INSTALL_PATH
+
 class QuranInstaller:
-    def __init__(self,reciter):
+    """This class handles the installing formatting hash verification and organizing of the audio obtained from the zip file."""
+
+    def __init__(self, reciter):
         self.reciter = reciter
         self.output_path = INSTALL_PATH
+
     def download(self):
+        """Downloads zips based off of the selected reciter"""
 
 
-        # choosing reciter to download
+        # Choosing reciter to download
         match self.reciter:
             case "Abdul_Basit_Mujawwad":
                 url = "https://everyayah.com/data/Abdul_Basit_Mujawwad_128kbps/000_versebyverse.zip"
@@ -34,10 +40,15 @@ class QuranInstaller:
             for chunk in response.iter_content(chunk_size=8192):
                 file.write(chunk)
 
-    def  extract(self):
-        with zipfile.ZipFile(os.path.join("audio","audio.zip"),"r") as zip_ref:
+    def extract(self):
+        """Extracts the downloaded zip"""
+
+        with zipfile.ZipFile(os.path.join(self.output_path, "audio.zip"),"r") as zip_ref:
             zip_ref.extractall(self.output_path)
+
     def check_hash(self):
+        """Checks hashes based off of the hash file that is given after extracting the zip file."""
+
         check_sum = os.path.join(self.output_path,"000_checksum.md5")
         with open(check_sum,"r") as file:
             hashes = []
@@ -45,38 +56,30 @@ class QuranInstaller:
                 line =  line.split()
                 file_hash = line[0]
                 hashes.append(file_hash)
-        count = 0
         gen_hashes = []
-        for mp3 in os.listdir(self.output_path):
-            file_path = os.path.join(self.output_path,mp3)
-            if not mp3.lower().endswith(".mp3"):
+        for filename in os.listdir(self.output_path):
+            file_path = os.path.join(self.output_path,filename)
+            if not filename.lower().endswith(".mp3"):
                 continue
-            # if os.path.isfile(file_path):
-            hash = hashlib.md5()
+            # If os.path.isfile(file_path):
+            md5_hash = hashlib.md5()
             with open(file_path, "rb") as f:
                 for chunk in iter(lambda: f.read(4096), b""):
-                    hash.update(chunk)
-            generated_hash = hash.hexdigest()
-            count += 1
+                    md5_hash.update(chunk)
+            generated_hash = md5_hash.hexdigest()
             gen_hashes.append(generated_hash)
             
         return(gen_hashes == hashes)
 
-    
-            
-
-                    
-                    
-                    
-        
-        
-        
     def rename(self):      
+        """Renames each file into the chapter|verse.mp3 format."""
+
         directory_path = self.output_path
 
         for filename in os.listdir(directory_path):
             if not filename.lower().endswith(".mp3"):
                 continue
+
             chapter_number = filename[:3]
             verse_number = int(filename[3:-4])
             new_filename = f"{int(chapter_number)}|{int(verse_number)}.mp3"
@@ -85,43 +88,39 @@ class QuranInstaller:
             newpath = os.path.join(directory_path,new_filename)
 
             os.rename(oldpath,newpath) 
-    def   organize(self):
+
+    def organize(self):
+        """Organize each file into its own folder based on the chapter name."""
+
         directory_path = self.output_path
         new_folders = []
 
-        for filename in os.listdir(directory_path): # this is finding all of the names 
+        for filename in os.listdir(directory_path): # This is finding all of the names 
             chapter_number = ""
             if not filename.lower().endswith(".mp3"):
                 continue
             for char in filename:
-                if char != "|":                 # this is finding the  number of the chpater
+                if char != "|":                 # This is finding the  number of the chpater
                     chapter_number += char
                 else:
-                        break
+                    break
             
 
-            chapter_number = int(chapter_number)    # makes it a int so it can be used ot make folders
+            chapter_number = int(chapter_number)    # Makes it a int so it can be used ot make folders
 
-            if chapter_number not in new_folders:   # if the chpater isnt included add it
+            if chapter_number not in new_folders:   # If the chpater isnt included add it
                 new_folders.append(chapter_number)
-            else:
-                pass
         print(new_folders)
 
-        # # this is going to make all of the folders for each name in the list
+        # This is going to make all of the folders for each name in the list
         for chapter_number in new_folders:
-            os.makedirs(f"{directory_path}/{chapter_number}",exist_ok=True) # exist to prevent erros if the folder is already there
+            os.makedirs(f"{directory_path}/{chapter_number}",exist_ok=True) # Exist to prevent erros if the folder is already there
 
-        # files = [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
-
-        # print(files)
-
-
-        for filename in os.listdir(directory_path): # this is finding all of the names 
+        for filename in os.listdir(directory_path): # This is finding all of the names 
             chapter_number = ""
             dirs = [d for d in os.listdir(directory_path) if os.path.isdir(os.path.join(directory_path,d))]
             for char in filename:
-                if char != "|":                 # this is finding the  number of the chpater
+                if char != "|":                 # This is finding the  number of the chpater
                     chapter_number += char
                 else:
                     break
@@ -129,6 +128,8 @@ class QuranInstaller:
                 if d == chapter_number:
                     shutil.move(os.path.join(directory_path, filename), os.path.join(directory_path, d))   
     def run(self):
+        """Downloads zip extracts it checks the hash renames all of the files and organizes them into folders based on the chapters they belong too."""
+
         self.download()
         self.extract()
         if self.check_hash():
@@ -136,7 +137,7 @@ class QuranInstaller:
             self.rename()
             self.organize()
         else:
-            print("some hashes didnt match")
+            print("some hashes didn't match")
             
         
         
---
+++ player.py
@@ -0,0 +1,25 @@
+import pygame
+
+class Player:
+    def __init__(
+    self, 
+    interval = 0,
+    speed = 1.0,
+    repeat = False,
+    waitpause = Falsem,
+    pygame.mixer.init()
+    ):
+
+        self.interval = interval
+        self.speed = speed
+        self.repeat = repeat
+        self.waitpause = waitpause
+
+    def play_range(self):
+        pass
+    def play_surah(self):
+        pass
+    def pause(self):
+        pass
+    def stop(self):
+        pass
